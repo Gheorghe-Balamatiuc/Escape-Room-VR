39955ea6e875befdbd3ba6165dad8b117e8d9239
Gheorghe-Balamatiuc <127909548+Gheorghe-Balamatiuc@users.noreply.github.com>
2025-05-11 09:49:48 +0300

    Fixed NextClip function;

--- Assets/_Course Library/Scripts/Actions/PlayVideo.cs
+++ Assets/_Course Library/Scripts/Actions/PlayVideo.cs
@@ -56,6 +56,8 @@ public class PlayVideo : MonoBehaviour
     public void NextClip()
     {
         index = ++index % videoClips.Count;
+        Debug.Log(index);
+        videoPlayer.clip = videoClips[index];
         Play();
     }
 
