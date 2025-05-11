9d0b96a1f488ca3dd2649ee1114d9913820b44f4
Gheorghe-Balamatiuc <127909548+Gheorghe-Balamatiuc@users.noreply.github.com>
2025-05-11 17:16:54 +0300

    Added buttons for hints.

---
+++ Assets/_Course Library/Scripts/Actions/HintButtonPress.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+    public void Log()
+    {
+        Debug.Log("press");
+    }
+}
---
+++ Assets/_Course Library/Scripts/Actions/HintButtonPress1.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress1 : MonoBehaviour
+{
+    public void Log()
+    {
+        Debug.Log("press");
+    }
+}
